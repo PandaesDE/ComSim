5d00d13c049726c1f40d34f8b7e02a0be212321a
GiimPy <GiimPy159@gmail.com>
2023-05-27 21:23:47 +0200

    changes: - created own directory for map generation - introduced map styles

--- Assets/Scripts/MapGenerator/MapGenerator.cs
+++ Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -30,16 +30,18 @@ public class MapGenerator : MonoBehaviour
     private Color[] pix;
     private SpriteRenderer rend;
 
-    private Gradient mapGradient;
+
+    private MapStyle mapStyle;
+
 
     void Start()
     {
-        initializeMapGradient();
         persistence = -.5f;
         frequency = .6f;
         octaves = 8;
         amplitude = 1;
         rend = GetComponent<SpriteRenderer>();
+        mapStyle = new MapStyle();
 
         // Set up the texture and a Color array to hold pixels during processing.
         noiseTex = new Texture2D(pixWidth, pixHeight);
@@ -48,33 +50,7 @@ public class MapGenerator : MonoBehaviour
 
     }
 
-    void initializeMapGradient()
-    {
-        mapGradient = new Gradient();
-        GradientColorKey[] colorKey = new GradientColorKey[8];
-        GradientAlphaKey[] alphaKey = new GradientAlphaKey[8];
-        int index = 0;
-
-        addColorKey(new Color32(19, 49, 138, 255), 0.0f);       // Water dark
-        addColorKey(new Color32(135, 170, 232, 255), 0.2f);     // Water light
-        addColorKey(new Color32(247, 246, 195, 255), 0.21f);    // Sand
-        addColorKey(new Color32(121, 199, 99, 255), 0.3f);      // Grass
-        addColorKey(new Color32(79, 121, 66, 255), 0.6f);       // Grass dark
-        addColorKey(new Color32(87, 85, 80, 255), 0.61f);       // Stone
-        addColorKey(new Color32(163, 163, 163, 255), 0.8f);     // Mountain
-        addColorKey(Color.white, 1.0f);                         // Snow
-
-        mapGradient.SetKeys(colorKey, alphaKey);
-
-        void addColorKey(Color c, float t)
-        {
-            colorKey[index].color = c;
-            colorKey[index].time = t;
-            alphaKey[index].alpha = 1.0f;
-            alphaKey[index].time = t;
-            index++;
-        }
-    }
+    
 
     void CalcNoise()
     {
@@ -89,7 +65,7 @@ public class MapGenerator : MonoBehaviour
                 float xCoord = xOrg + x / noiseTex.width;
                 float yCoord = yOrg + y / noiseTex.height;
                 float sample = OctavePerlin(xCoord, yCoord);
-                pix[(int)y * noiseTex.width + (int)x] = mapGradient.Evaluate(sample);
+                pix[(int)y * noiseTex.width + (int)x] = mapStyle.getColor(sample, MapStyle.mapStyles.NORMAL);
                 x++;
             }
             y++;
---
+++ Assets/Scripts/MapGenerator/MapStyle.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapStyle
+{
+    //used colors
+    private Dictionary<mapLayers, Color> mapColors;
+
+    private Gradient mapGradient;
+
+    private enum mapLayers
+    {
+        DARK_WATER,
+        BRIGHT_WATER,
+        SAND,
+        BRIGHT_GRASS,
+        DARK_GRASS,
+        BRIGHT_STONE,
+        DARK_STONE,
+        SNOW
+    }
+
+    public enum mapStyles
+    {
+        NORMAL,
+        GRADIENT
+    }
+
+    public MapStyle()
+    {
+        initializeMapColors();
+        initializeMapGradient();
+    }
+
+    public Color getColor(float sample, mapStyles ms)
+    {
+        if (ms.Equals(mapStyles.GRADIENT))
+        {
+            return getGradientColor(sample);
+        }
+
+        return getNormalColor(sample); ;
+    }
+    #region
+    private Color getNormalColor(float sample)
+    {
+        if (sample < .1f)
+        {
+            return mapColors[mapLayers.DARK_WATER];
+        }
+        if (sample < .2f)
+        {
+            return mapColors[mapLayers.BRIGHT_WATER];
+        }
+        if (sample < .23f)
+        {
+            return mapColors[mapLayers.SAND];
+        }
+        if (sample < .35f)
+        {
+            return mapColors[mapLayers.BRIGHT_GRASS];
+        }
+        if (sample < .6f)
+        {
+            return mapColors[mapLayers.DARK_GRASS];
+        }
+        if (sample < .65f)
+        {
+            return mapColors[mapLayers.BRIGHT_STONE];
+        }
+        if (sample < .8f)
+        {
+            return mapColors[mapLayers.DARK_STONE];
+        }
+        return mapColors[mapLayers.SNOW];
+    }
+    #endregion
+
+    #region Gradient
+    private Color getGradientColor(float sample)
+    {
+        return mapGradient.Evaluate(sample);
+    }
+
+    void initializeMapGradient()
+    {
+        mapGradient = new Gradient();
+        GradientColorKey[] colorKey = new GradientColorKey[8];
+        GradientAlphaKey[] alphaKey = new GradientAlphaKey[8];
+        int index = 0;
+
+        addColorKey(mapColors[mapLayers.DARK_WATER], 0.0f);
+        addColorKey(mapColors[mapLayers.BRIGHT_WATER], 0.2f);
+        addColorKey(mapColors[mapLayers.SAND], 0.21f);
+        addColorKey(mapColors[mapLayers.BRIGHT_GRASS], 0.3f);
+        addColorKey(mapColors[mapLayers.DARK_GRASS], 0.6f);
+        addColorKey(mapColors[mapLayers.BRIGHT_STONE], 0.61f);
+        addColorKey(mapColors[mapLayers.DARK_STONE], 0.8f);
+        addColorKey(mapColors[mapLayers.SNOW], 1.0f);
+
+        mapGradient.SetKeys(colorKey, alphaKey);
+
+        void addColorKey(Color c, float t)
+        {
+            colorKey[index].color = c;
+            colorKey[index].time = t;
+            alphaKey[index].alpha = 1.0f;
+            alphaKey[index].time = t;
+            index++;
+        }
+    }
+    #endregion
+
+    private void initializeMapColors()
+    {
+        mapColors = new Dictionary<mapLayers, Color>();
+        mapColors.Add(mapLayers.DARK_WATER, new Color32(19, 49, 138, 255));
+        mapColors.Add(mapLayers.BRIGHT_WATER, new Color32(135, 170, 232, 255));
+        mapColors.Add(mapLayers.SAND, new Color32(247, 246, 195, 255));
+        mapColors.Add(mapLayers.BRIGHT_GRASS, new Color32(121, 199, 99, 255));
+        mapColors.Add(mapLayers.DARK_GRASS, new Color32(79, 121, 66, 255));
+        mapColors.Add(mapLayers.BRIGHT_STONE, new Color32(87, 85, 80, 255));
+        mapColors.Add(mapLayers.DARK_STONE, new Color32(163, 163, 163, 255));
+        mapColors.Add(mapLayers.SNOW, new Color32(255, 255, 255, 255));
+
+    }
+}
